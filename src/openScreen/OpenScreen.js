5b6539bdae90341ec38243c1457a11df487b6683
adi604 <84729170+adi604@users.noreply.github.com>
2022-03-28 18:17:23 +0300

    login
    login

--- src/openScreen/OpenScreen.js
+++ src/openScreen/OpenScreen.js
@@ -2,7 +2,7 @@ function OpenScreen() {
     return (
         <form>
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <label for="exampleInputEmail1" class="form-label">Username</label>
                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
                     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
@@ -10,11 +10,12 @@ function OpenScreen() {
                 <label for="exampleInputPassword1" class="form-label">Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword1"></input>
             </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            <div>
+            <label>Not register?&nbsp;</label>
+            <a href="url">Click here</a>
+            <label>&nbsp;to register</label>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Login</button>
         </form>
     );
 }
