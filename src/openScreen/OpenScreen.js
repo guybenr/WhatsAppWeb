96d07ffacc4a368622db23958bf578d8a719a69b
adi604 <84729170+adi604@users.noreply.github.com>
2022-03-29 00:22:28 +0300

    update
    update

--- src/openScreen/OpenScreen.js
+++ src/openScreen/OpenScreen.js
@@ -4,18 +4,18 @@ function OpenScreen() {
             <form>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Username</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                    <input type="email" class="form-control username" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email /&nbsp;Username"></input>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1"></input>
+                    <input type="password" class="form-control password" id="exampleInputPassword1" placeholder="Password"></input>
                 </div>
                 <div>
                     <label>Not register?&nbsp;</label>
                     <a href="url">Click here</a>
                     <label>&nbsp;to register</label>
                 </div>
-                <button type="submit" class="btn btn-primary">Login</button>
+                <button type="submit" class="btn btn-primary">Log in</button>
             </form>
         </div>
 
