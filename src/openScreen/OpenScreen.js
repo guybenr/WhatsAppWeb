af3ad099e9dbaaa9ee5e604bb838663ed63d0830
adi604 <84729170+adi604@users.noreply.github.com>
2022-03-28 19:36:15 +0300

    last
    last

--- src/openScreen/OpenScreen.js
+++ src/openScreen/OpenScreen.js
@@ -1,22 +1,24 @@
 function OpenScreen() {
     return (
-        <form>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Username</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1"></input>
-            </div>
-            <div>
-            <label>Not register?&nbsp;</label>
-            <a href="url">Click here</a>
-            <label>&nbsp;to register</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Login</button>
-        </form>
+        <div className="home">
+            <form>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Username</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1"></input>
+                </div>
+                <div>
+                    <label>Not register?&nbsp;</label>
+                    <a href="url">Click here</a>
+                    <label>&nbsp;to register</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+
     );
 }
 
