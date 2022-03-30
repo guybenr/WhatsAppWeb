54f78bbfad55cc45eed18ecb5967f0b339d19854
guybenr <guybenr@hotmail.com>
2022-03-30 22:58:43 +0300

    changes new1

--- src/openScreen/OpenScreen.js
+++ src/openScreen/OpenScreen.js
@@ -1,45 +1,54 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import UsersList from "../usersList/UsersList";
+import { Modal } from "bootstrap";
 
 
-function OpenScreen() {
-
+function OpenScreen(props) {
     const navigate = useNavigate();
-
-    const handleSubmit = () => {
+    const [isInvalid, setIsInvalid] = React.useState(false);
+    const handleSubmit = (event) => {
+        event.preventDefault();
         var username = document.getElementById('exampleInputEmail1').value;
         var password = document.getElementById('exampleInputPassword1').value;
         for (let i = 0; i < UsersList.length; i++) {
             if (UsersList[i].userName === username && UsersList[i].password === password) {
+                props.setUserLoginDetails({ username: { username }, password })
                 navigate('/Chat');
                 return;
             }
-    
         }
-        this.setState({ isLoggedIn: false });
+        setIsInvalid(true);
     }
 
 
     return (
-        <div className="home">
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Username</label>
-                    <input className="form-control username" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email /&nbsp;Username"></input>
-                </div>
-                <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control password" id="exampleInputPassword1" placeholder="Password"></input>
-                </div>
-                <div>
-                    <label>Not register?&nbsp;</label>
-                    <Link to='/Register'>Click here</Link>
-                    <label>&nbsp;to register</label>
-                </div>
-                <button type="submit" className="btn btn-primary">Log in</button>
-            </form>
-        </div>
+        <>
+            {isInvalid == true && 
+            <Modal show={isInvalid}>
+                <Modal.Header>Hi</Modal.Header>
+                <Modal.Body>asdfasdf</Modal.Body>
+                <Modal.Footer>This is the footer</Modal.Footer>
+            </Modal>}
+            <div className="home" id="main-div">
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Username</label>
+                        <input className="form-control username" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email /&nbsp;Username"></input>
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputPassword1" className="form-label">Password</label>
+                        <input type="password" className="form-control password" id="exampleInputPassword1" placeholder="Password"></input>
+                    </div>
+                    <div>
+                        <label>Not register?&nbsp;</label>
+                        <Link to='/Register'>Click here</Link>
+                        <label>&nbsp;to register</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Log in</button>
+                </form>
+            </div>
+        </>
     );
 };
 
